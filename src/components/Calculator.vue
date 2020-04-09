9a1fa790d9ea3ee1437aefb14a79c9bc5008cce2
Derek Johnston <Derek@dj-design.ca>
2020-04-09 13:47:21 -0400

    Removed nested if and added adding to totals

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,34 +5,34 @@
         .card.m-2
           .row.m-1
             .col-12
-              b-form-input(type="number" v-model="value")
+              b-form-input(type="number" v-model="input")
           .row.m-1
             .col-3
-              b-button(@click="valueAdd('7')") 7
+              b-button(@click="addInput('7')") 7
             .col-3
-              b-button(@click="valueAdd('8')") 8
+              b-button(@click="addInput('8')") 8
             .col-3
-              b-button(@click="valueAdd('9')") 9
+              b-button(@click="addInput('9')") 9
             .col-3
-              b-button(variant="danger" @click="clear") cls
+              b-button(variant="danger" @click="cls") CLS
           .row.m-1
             .col-3
-              b-button(@click="valueAdd('4')") 4
+              b-button(@click="addInput('4')") 4
             .col-3
-              b-button(@click="valueAdd('5')") 5
+              b-button(@click="addInput('5')") 5
             .col-3
-              b-button(@click="valueAdd('6')") 6
+              b-button(@click="addInput('6')") 6
             .col-3
-              b-button(@click="step('-')") -
+              b-button(@click="addAction('-')") -
           .row.m-1
             .col-3
-              b-button(@click="valueAdd('1')") 1
+              b-button(@click="addInput('1')") 1
             .col-3
-              b-button(@click="valueAdd('2')") 2
+              b-button(@click="addInput('2')") 2
             .col-3
-              b-button(@click="valueAdd('3')") 3
+              b-button(@click="addInput('3')") 3
             .col-3
-              b-button(@click="step('+')") +
+              b-button(@click="addAction('+')") +
           .row.m-1
             .col-9
               | {{total}}
@@ -45,53 +45,55 @@
 export default {
   data() {
     return {
-      value: 0,
+      input: 0,
       total: null,
-      steps: []
+      steps: [{ value: 0, action: "" }],
     };
   },
   methods: {
-    step(action) {
-      this.steps.push(this.value);
-      this.steps.push(action);
-      this.value = 0;
-    },
-    clear() {
-      this.value = 0;
-      this.total = null;
-      this.steps = [];
-    },
-    valueAdd(value) {
-      if (this.value === 0) {
-        this.value = value;
+    addInput(newInput) {
+      if (this.input === 0) {
+        this.input = newInput;
       } else {
-        this.value = this.value + value;
+        this.input += newInput;
       }
     },
+    addAction(action) {
+      if (this.input != 0) {
+        this.addStep(this.input, action);
+      } else if (this.total) {
+        this.addStep(this.total, action);
+      }
+    },
+    addStep(value, action) {
+      this.steps[this.steps.length - 1].value = value;
+      this.steps.push({ value: 0, action: action });
+      this.total = null;
+      this.input = 0;
+    },
+    cls() {
+      this.clear();
+      this.total = null;
+    },
+    clear() {
+      this.input = 0;
+      this.steps = [{ value: 0, action: "" }];
+    },
     calculate() {
-      this.steps.push(this.value);
-      let subtotal = 0;
-      let action = null;
+      this.steps[this.steps.length - 1].value = this.input;
       this.steps.forEach(step => {
-        if (step === "+") {
-          action = step;
-        } else if (step === "-") {
-          action = step;
-        } else {
-          if (action === null) {
-            subtotal = step;
-          } else {
-            if (action === "+") {
-              subtotal = parseFloat(subtotal) + parseFloat(step);
-            } else if (action === "-") {
-              subtotal = parseFloat(subtotal) - parseFloat(step);
-            }
-          }
+        switch (step.action) {
+          case "+":
+            this.total = parseFloat(this.total) + parseFloat(step.value);
+            break;
+          case "-":
+            this.total = parseFloat(this.total) - parseFloat(step.value);
+            break;
+          default:
+            this.total = parseFloat(step.value);
         }
       });
-
       this.clear();
-      this.total = subtotal;
     }
   }
 };
